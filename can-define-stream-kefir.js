4f8e1b101a08b5a9f4fdafa15b535c25828f094e
Obaid Ahmed <obaid@bitovi.com>
2017-04-24 14:03:48 -0400

    Fixing the namespace

--- can-define-stream-kefir.js
+++ can-define-stream-kefir.js
@@ -2,4 +2,4 @@ var namespace = require('can-util/namespace');
 var canDefineStream = require('can-define-stream');
 var canStreamKefir = require('can-stream-kefir');
 
-module.exports = namespace.canDefineStreamKefir = canDefineStream(canStreamKefir);
+module.exports = namespace.defineStreamKefir = canDefineStream(canStreamKefir);
